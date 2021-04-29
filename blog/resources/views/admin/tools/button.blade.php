1175e7cae0cf5d3e4305740d38ab7b4365f9e988
许程 <1439946976@qq.com>
2021-04-30 01:44:57 +0800

    移除多余

--- blog/resources/views/admin/tools/button.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="btn-group filter-button-group dropdown">
-    <a class="btn btn-sm btn-default pull-right" href="{{$url}}" rel="external nofollow" ><i class="fa {{$icon}}"></i> {{$text}}</a>
-</div>
